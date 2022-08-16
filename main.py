59ebb6604587cecc579533a7c9d0c6555c53a9fd
Hector Torres <hectortorres770@gmail.com>
2022-08-16 00:52:13 -0400

    Second version
    This version add:
    - the album cover is automatically added to the mp3 file
    - you can modify the title and the author of the song from the script

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pytube import YouTube #pip install pytube3
 import moviepy.editor as mp #pip install moviepy
+from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, APIC
 from unicodedata import normalize
 import subprocess
 import requests
@@ -39,6 +41,15 @@ while not success:
 		print("\n**An error occurred!**\n")
 		success = False
 
+# ---------- Thumbnail download ---------- #
+res = requests.get(thumbnail,stream = True)
+thumbnail_name = "cover.png"
+if res.status_code == 200:
+    with open(thumbnail_name,'wb') as f:
+        shutil.copyfileobj(res.raw, f)
+else:
+    print('\n**Could not download thumbnail!**\n')
+
 # ---------- The video is converted to mp3 - 320kbps and deleted ---------- #
 print("Converting to mp3")
 clip = mp.VideoFileClip(downloaded_file)
@@ -47,6 +58,24 @@ clip.close()
 os.remove(downloaded_file)
 print("Converted!\n")
 
+# ---------- MP3 Edit ---------- #
+mp3_route = os.path.basename(f"{base}.mp3")
+edit = ""
+while not (edit =="s" or edit =="n"):
+	edit = input("Do you want to edit your mp3 file? [s/n]: ")
+
+if(edit.lower() == "s"):
+	audio = ID3(mp3_route)
+	with open(thumbnail_name, 'rb') as albumart:
+		audio['APIC'] = APIC(mime = thumbnail_name, type = 3, 
+							desc = u"Cover", data = albumart.read())
+	audio.save()
+	title = input("Enter the title of song: ")
+	author = input("Enter the author of song: ")
+	audio = EasyID3(mp3_route)
+	audio['title'] = title
+	audio['artist'] = author
+	audio.save()
 
 # ---------- We look for the directory ---------- #
 src = base + ".mp3"
@@ -69,24 +98,18 @@ if artist_path == "":
 	print("Created a new directory")
 dest = artist_path
 
-# ---------- Thumbnail download ---------- #
-res = requests.get(thumbnail,stream = True)
-thumbnail_name = "cover.png"
-if res.status_code == 200:
-    with open(thumbnail_name,'wb') as f:
-        shutil.copyfileobj(res.raw, f)
+
+
+# ---------- Move files and open the new directory ---------- #
+if edit.lower() == "n":
+	#We move the thumbnail
+	shutil.move("cover.png", dest)
 else:
-    print('\n**Could not download thumbnail!**\n')
+	os.remove(thumbnail_name)
 
-#We move the thumbnail
-shutil.move("cover.png", dest)
-
-# ---------- Move the mp3 and open the new directory ---------- #
 shutil.move(src, dest)
 explorer_path = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
 subprocess.run([explorer_path, dest])
-#Optional (TagScanner is used to modify the data of an mp3 )
-# subprocess.Popen("C:\Program Files\TagScanner\Tagscan.exe")
 
 
 
